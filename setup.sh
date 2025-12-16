1e038e13792e336d899c89ea5bf01a364540f768
Venkatesh2304 <96430022+Venkatesh2304@users.noreply.github.com>
2025-12-16 20:54:19 +0530

    s

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ PORT=8000
 git pull -ff
 FRONTEND_DIR="$(cd "$(dirname "$0")" && pwd)"
 pm2 delete frontend | true
-pm2 start "serve $FRONTEND_DIR/dist -l $PORT" --name frontend
+pm2 start "serve -s $FRONTEND_DIR/dist -l $PORT" --name frontend
 pm2 save
